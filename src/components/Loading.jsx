b316cc783938ba24a49a06dc5e0007c061e39d59
MüminErsahin <75380505+MuminErsahin@users.noreply.github.com>
2024-07-18 17:31:31 +0300

    Loading Değişti

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,34 +3,12 @@
 function Loading() {
   return (
     <div className="flex justify-center items-center ">
- <div className="loader">
-  <div className="box box0">
-    <div></div>
+ <div className="container">
+  <div className="folder">
+    <div className="top"></div>
+    <div className="bottom"></div>
   </div>
-  <div className="box box1">
-    <div></div>
-  </div>
-  <div className="box box2">
-    <div></div>
-  </div>
-  <div className="box box3">
-    <div></div>
-  </div>
-  <div className="box box4">
-    <div></div>
-  </div>
-  <div className="box box5">
-    <div></div>
-  </div>
-  <div className="box box6">
-    <div></div>
-  </div>
-  <div className="box box7">
-    <div></div>
-  </div>
-  <div className="ground">
-    <div></div>
-  </div>
+  <div className="title">getting files ready...</div>
 </div>
 </div>
   )
