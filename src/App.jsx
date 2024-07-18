5ffd942b4ce0429e48cccdab9c54f9093cd4257e
MüminErsahin <75380505+MuminErsahin@users.noreply.github.com>
2024-07-18 17:22:24 +0300

    Loading eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { useDispatch, useSelector } from 'react-redux'
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 import Footer from './components/Footer'
@@ -20,7 +20,36 @@ useEffect(() => {
 },[])
 
 
-if(data===null) return <div>Loading</div>
+if(data===null) return(<div className="loader">
+  <div className="box box0">
+    <div></div>
+  </div>
+  <div className="box box1">
+    <div></div>
+  </div>
+  <div className="box box2">
+    <div></div>
+  </div>
+  <div className="box box3">
+    <div></div>
+  </div>
+  <div className="box box4">
+    <div></div>
+  </div>
+  <div className="box box5">
+    <div></div>
+  </div>
+  <div className="box box6">
+    <div></div>
+  </div>
+  <div className="box box7">
+    <div></div>
+  </div>
+  <div className="ground">
+    <div></div>
+  </div>
+</div>
+)
   return (
     <>
     <div className={darkMode ? "dark" : ""}>
